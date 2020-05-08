5066f18f0071bddc855a67a3c796c78948b99872
Oskar Eriksson <oseriks@kth.se>
2020-05-08 10:29:50 +0200

    Colorize directory in batches

--- colorize_dir.py
+++ colorize_dir.py
@@ -1,4 +1,5 @@
 import os
+import math
 import warnings
 import torch
 import numpy as np
@@ -6,9 +7,31 @@ from glob import glob
 from tqdm import tqdm
 from argparse import ArgumentParser
 from skimage import io, transform
-from colorize import network, dataset, util
+from colorize import network, util
 
-input_size = (224, 224)
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, paths, in_size=(224, 224), out_size=(56, 56)):
+        self.paths = paths
+        self.in_size = in_size
+        self.out_size = out_size
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        L, Y = util.imread(path, size=None)
+        X = (transform.resize(L.copy(), self.in_size) - 50) / 50
+        Z = util.soft_encode(transform.resize(Y, self.in_size))
+        X = X.transpose(2, 0, 1).astype(np.float32)
+        Z = Z.transpose(2, 0, 1).astype(np.float32)
+        return X, Z, L, path
+
+def collate_fn(batch):
+    Xs, Zs, Ls, paths = zip(*batch)
+    Xs = torch.utils.data.dataloader.default_collate(Xs)
+    Zs = torch.utils.data.dataloader.default_collate(Zs)
+    return Xs, Zs, Ls, paths
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='Copies a directory structure with colorized images.')
@@ -16,9 +39,16 @@ if __name__ == '__main__':
     parser.add_argument('in_dir')
     parser.add_argument('out_dir')
     args = parser.parse_args()
-    images = glob(os.path.join(args.in_dir, '**', '*.jpg'), recursive=True)
+    images = []
+    for ext in ['jpg', 'JPEG']:
+        images.extend(glob(os.path.join(args.in_dir, '**', f'*.{ext}'), recursive=True))
     model_name = os.path.splitext(os.path.split(args.model)[1])[0]
     out_dir = os.path.join(args.out_dir, model_name)
+    for image in images:
+        # Create directory if it does not exits
+        path = os.path.split(image)[0]
+        path = os.path.join(out_dir, path)
+        os.makedirs(path, exist_ok=True)
 
     # Load model
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -28,25 +58,19 @@ if __name__ == '__main__':
     cnn.to(device)
     w = torch.tensor(np.load('resources/w.npy')).to(device)
 
-    for i, image in tqdm(enumerate(images), total=len(images)):
-        # Create directory if it does not exits
-        path, name = os.path.split(image)
-        path = os.path.join(out_dir, path)
-        os.makedirs(path, exist_ok=True)
+    # Create DataLoader
+    batch_size = 32
+    data = Dataset(images)
+    dataloader = torch.utils.data.DataLoader(data,
+                                            batch_size=batch_size,
+                                            shuffle=False,
+                                            num_workers=8,
+                                            collate_fn=collate_fn)
 
-        # Load, normalize, encode and resize
-        L, Y = util.imread(image, size=None)
-        X = (transform.resize(L.copy(), input_size) - 50) / 50
-        Z = util.soft_encode(transform.resize(Y, input_size))
-
-        # Convert to PyTorch tensor
-        X = torch.tensor(X.transpose(2, 0, 1).astype(np.float32)[np.newaxis,...]).to(device)
-        Z = torch.tensor(Z.transpose(2, 0, 1).astype(np.float32)[np.newaxis,...]).to(device)
-
-        # Colorize, decode, and resize
-        Z_hat = util.reshape(cnn(X).cpu().data.numpy(), 3)[0]
-        ab = transform.resize(util.decode(Z_hat, strategy='annealed_mean'), L.shape[:2])
-
-        # Save
-        rgb = (util.lab2rgb(L, ab) * 255).astype(np.uint8)
-        io.imsave(os.path.join(path, name), rgb)
+    for batch, (Xs, Zs, Ls, paths) in tqdm(enumerate(dataloader), total=math.ceil(len(data) / batch_size)):
+        Z_hats = cnn(Xs).cpu().data.numpy()
+        for Z_hat, L, path in zip(Z_hats, Ls, paths):
+            Z_hat = Z_hat.transpose(1, 2, 0)
+            ab = transform.resize(util.decode(Z_hat, strategy='annealed_mean'), L.shape[:2])
+            rgb = (util.lab2rgb(L, ab) * 255).astype(np.uint8)
+            io.imsave(os.path.join(out_dir, path), rgb)
